e924191192119bb6bf790283c5b5001148bf7427
Josh Nielsen <josh@joshontheweb.com>
2013-04-07 13:39:42 -0600

    added keyboard.

---
+++ wavyjones.js
@@ -0,0 +1,45 @@
+var WavyJones = function (context, elem) {
+	var analyser = context.createAnalyser();
+
+	analyser.width = document.getElementById(elem).offsetWidth;
+	analyser.height = document.getElementById(elem).offsetHeight;
+	analyser.lineColor = 'yellow';
+	analyser.lineThickness = 5;
+
+    var paper = Raphael(elem, analyser.width, analyser.height),
+        oscLine = paper.path([['M', 0, analyser.height/2], ['L', analyser.width, analyser.height/2], 'Z']),
+        noDataPoints = 10,
+		freqData = new Uint8Array(analyser.frequencyBinCount);
+
+    oscLine.attr({stroke: analyser.lineColor, 'stroke-width': analyser.lineThickness});
+
+    var drawLine = function () {
+        analyser.getByteTimeDomainData(freqData);
+
+        var graphPoints = [],
+            graphStr = '';
+
+        graphPoints.push('M0, ' + (analyser.height/2));
+
+        for (var i = 0; i < freqData.length; i++) {
+            if (i % noDataPoints) {
+                var point = (freqData[i] / 128) * (analyser.height / 2);
+                graphPoints.push('L' + i + ', ' + point); 
+            }
+        }
+
+        for (i = 0; i < graphPoints.length; i++) {
+            graphStr += graphPoints[i];
+        }
+
+        oscLine.attr('stroke', analyser.lineColor);
+        oscLine.attr('stroke-width', analyser.lineThickness);
+        oscLine.attr('path', graphStr);
+
+        setTimeout(drawLine, 5);
+    };
+
+    drawLine();
+
+    return analyser;
+};
