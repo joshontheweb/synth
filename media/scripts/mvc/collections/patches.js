8e4317a844141307f91f29f4c5c1c2632ca481b9
Josh Nielsen <josh@joshontheweb.com>
2013-05-27 17:52:22 -0700

    set to point xhr requests to soundkeep.com

--- media/scripts/mvc/collections/patches.js
+++ media/scripts/mvc/collections/patches.js
@@ -2,7 +2,7 @@
   'use strict';
 
   bs.collections.Patches = Backbone.Collection.extend({
-    url: 'http://localhost:1337/synthpatches',
+    url: 'http://soundkeep.com/synthpatches',
     model: bs.models.Patch
   });
 })();
