302bd9716b9663cc3f4e69bb72b0a4d99f0d25e2
Josh Nielsen <josh@joshontheweb.com>
2013-10-04 18:49:34 -0600

    added gain knob to cv patches

--- media/scripts/mvc/models/cv_patch.js
+++ media/scripts/mvc/models/cv_patch.js
@@ -7,9 +7,12 @@
       this.sources = this.collection.patchSources;
       this.destinations = this.collection.patchDestinations;
       this.gainNode = this.context.createGainNode();
+      this.destination = this.destinations[this.get('destination')].node;
 
       this.on('change:source', this.sourceChange);
       this.on('change:destination', this.destinationChange);
+      this.on('change:gain', this.gainChange);
+      
       this.listenTo(this.collection, 'reset', this.exit);
 
       this.setSource(this.get('source'));
@@ -17,6 +20,7 @@
     },
 
     defaults: {
+      'gain': 0,
       'destination': 'none',
       'source': 'none',
       'outputIndex': 0
@@ -45,7 +49,7 @@
       }
 
       if (destination) {
-        this.gainNode.gain.value = (destination.maxValue > 20000 ? 20000 : destination.maxValue) / 2;
+        this.set({gain: (destination.maxValue > 20000 ? 20000 : destination.maxValue) / 2});
         this.gainNode.connect(destination);
       }
     },
@@ -65,10 +69,14 @@
       if (destination) {
         // set to max of 20000 or maxValue 
         // for some reason maxValue on oscillator.frequency is 100000.  this is to combat that.
-        this.gainNode.gain.value = (destination.maxValue > 20000 ? 20000 : destination.maxValue) / 2;
+        this.set({gain: (destination.maxValue > 20000 ? 20000 : destination.maxValue) / 2});
         this.gainNode.connect(destination);
       }
+    },
+
+    gainChange: function(model, gain) {
+      console.log(gain);
+      this.gainNode.gain.value = gain;
     }
-    
   });
 })();
--- media/scripts/mvc/views/cv_patch_view.js
+++ media/scripts/mvc/views/cv_patch_view.js
@@ -2,6 +2,17 @@
   'use strict';
 
   bs.views.CVPatchView = Backbone.View.extend({
+    initialize: function() {
+      this.gainKnob = this.model.gainKnob = new bs.models.Knob({
+        min: 0,
+        max: this.model.destination.maxValue > 20000 ? 20000 : this.model.destination.maxValue,
+        value: this.model.get('gain'),
+        startDegree: -140
+      });
+      
+      this.listenTo(this.gainKnob, 'change:value', this.handleGainInput);
+    },
+    
     events: {
       'change .sources': 'handleSourceInput',
       'change .destinations': 'handleDestinationInput'
@@ -19,6 +30,10 @@
     handleDestinationInput: function(e) {
       this.$destinations.blur();
       this.model.set({destination: e.target.value});
+    },
+    
+    handleGainInput: function(knob, gain) {
+      this.model.set({gain: gain});
     },
 
     renderSources: function() {
@@ -40,14 +55,21 @@
         }
       }
     },
-
+    
+    renderKnob: function() {
+      this.gainKnobView = new bs.views.KnobView({model: this.gainKnob});
+      this.$gain.html(this.gainKnobView.render().el);
+    },
+    
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
       this.$sources = this.$('.sources');
       this.$destinations = this.$('.destinations');
+      this.$gain = this.$('.gain');
 
       this.renderSources();
       this.renderDestinations();
+      this.renderKnob();
 
       this.$sources.val(this.model.get('source'));
       this.$destinations.val(this.model.get('destination'));
